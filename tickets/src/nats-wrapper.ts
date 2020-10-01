70dcaff5f40fd56cbf2013a982c4f8fce05afaeb
Levente Danel <danellevente@yahoo.com>
2020-10-01 08:38:11 +0300

    increase timeout limit when connecting to nats streaming srv

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -12,7 +12,10 @@ class NatsWrapper {
   }
 
   connect(clusterId: string, clientId: string, url: string) {
-    this._client = nats.connect(clusterId, clientId, { url });
+    this._client = nats.connect(clusterId, clientId, {
+      url,
+      connectTimeout: 30 * 1000,
+    });
 
     return new Promise((resolve, reject) => {
       this.client.on("connect", () => {
