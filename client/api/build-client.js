da00640897eab9282ebdb71722f4bed82f4ee8b7
Levente Danel <danellevente@yahoo.com>
2020-10-01 08:36:58 +0300

    fix load balancer url

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ const BuildClient = ({ req }) => {
   if (typeof window === "undefined") {
     //server side
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://ingress-nginx.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
