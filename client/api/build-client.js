9650f82486dddbfca6e64ea9c3648739e881c919
Levente Danel <danellevente@yahoo.com>
2020-10-14 12:11:05 +0300

    fix baseURL

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,8 @@ const BuildClient = ({ req }) => {
   if (typeof window === "undefined") {
     //server side
     return axios.create({
-      baseURL: process.env.REACT_APP_BASE_URL,
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
